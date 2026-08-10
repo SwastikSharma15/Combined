80e7f57ad2057534d5357ecefa14ae257f39417f
SwastikSharma15 <sharmaspeedx29@gmail.com>
2026-08-10 13:46:08 +0530

    home indi

--- src/components/ios/home-indicator.tsx
+++ src/components/ios/home-indicator.tsx
@@ -16,19 +16,31 @@ export function HomeIndicator({ children }: HomeIndicatorProps) {
   // Handle swipe up gesture
   useEffect(() => {
     let touchStartY = 0
+    let isSwipingFromBottom = false
 
     const handleTouchStart = (e: TouchEvent) => {
       touchStartY = e.touches[0].clientY
+
+      if (containerRef.current) {
+        const rect = containerRef.current.getBoundingClientRect()
+        const relativeY = touchStartY - rect.top
+        isSwipingFromBottom = relativeY > rect.height * 0.85
+      } else {
+        const windowHeight = window.innerHeight;
+        isSwipingFromBottom = touchStartY > windowHeight * 0.85;
+      }
     }
 
     const handleTouchMove = (e: TouchEvent) => {
-      const windowHeight = window.innerHeight;
-      const isBottom20 = touchStartY > windowHeight * 0.85;
-
-      if (!isBottom20) return;
+      if (!isSwipingFromBottom) return;
 
       const touchY = e.touches[0].clientY
       const diff = touchStartY - touchY
+
+      // Prevent native scrolling if swiping up from bottom
+      if (diff > 0 && e.cancelable) {
+        e.preventDefault()
+      }
 
       // If swiped up more than 100px, go home
       if (diff > 100) {
@@ -38,8 +50,8 @@ export function HomeIndicator({ children }: HomeIndicatorProps) {
 
     const element = containerRef.current
     if (element) {
-      element.addEventListener("touchstart", handleTouchStart)
-      element.addEventListener("touchmove", handleTouchMove)
+      element.addEventListener("touchstart", handleTouchStart, { passive: false })
+      element.addEventListener("touchmove", handleTouchMove, { passive: false })
     }
 
     return () => {
@@ -56,7 +68,7 @@ export function HomeIndicator({ children }: HomeIndicatorProps) {
 
       {/* Home Indicator */}
       <div
-        className="absolute bottom-2 left-1/2 transform -translate-x-1/2 w-[134px] h-[5px] bg-black rounded-full z-50 opacity-30 cursor-pointer"
+        className="absolute bottom-2 left-1/2 transform -translate-x-1/2 w-[134px] h-[5px] bg-black rounded-full z-[10000] opacity-30 cursor-pointer pointer-events-auto"
         onClick={closeApp}
       />
     </div>
