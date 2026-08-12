a7337ab74aaf3755db58574e97c037ab18b0cea2
SwastikSharma15 <sharmaspeedx29@gmail.com>
2026-08-12 19:34:03 +0530

    pkg

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,6 @@ const nextConfig = {
     // Allow production builds to successfully complete even if
     // your project has type errors during this migration phase.
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   }
 };
 
